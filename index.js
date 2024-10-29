86e8b68a871799d689c74b3c9901876ecf0457dc
Yasas Lekamge <yasaslekamge@gmail.com>
2024-10-29 11:44:20 +0530

    Final Release.

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
 
+const API_URL = "https://calendarific.com/api/v2";
+const API_KEY = "LWivFMNEjFHIOs6UsZ9ryfD5bjuXeiSt";
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { content: "Information will appear here", holidays: [] });
+});
+
+app.post("/submit", async (req, res) => {
+    const { country, year } = req.body;
+
+    try {
+        const result = await axios.get(`${API_URL}/holidays?api_key=${API_KEY}&country=${country}&year=${year}`);
+
+        const holidays = result.data.response.holidays.map(holiday => ({
+            name: holiday.name,
+            description: holiday.description,
+            date: holiday.date.iso // ISO date string
+        }));
+
+        res.render("index.ejs", { holidays });
+    } catch (error) {
+        res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
 });
 
 app.listen(PORT, () => {
